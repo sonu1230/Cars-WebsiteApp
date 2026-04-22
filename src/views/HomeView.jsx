1bd9db77e55b0df3f35cd66793f56f4bdb7023f6
Sonu <sonukumari.mahatopanjiyar@dcmail.ca>
2026-04-21 20:44:59 -0400

    Update home button with description
    Reading live cars count from context, complete HomeView and description with function of dynamic car count and CTA buttons

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useItemsContext } from '../context/ItemsContext'
+
 export default function HomeView(){
+  //Reading live car count from context
+  const { items } = useItemsContext()
+
   return (
     <div className="p-4 bg-light rounded">
-      <h2 className="h5">TODO: Home</h2>
-      <p className="mb-0">Introduce your app and link to list/create.</p>
+      <h2 className="h5">CarVault</h2>
+      <p className="mb-0">Your personal car collection manager. Browse, add, edit and track your favourite cars.</p>
+      <div className="d-flex gap-2">
+        {/* items.length is live, reflects current localStorage data */}
+        <Link to="/list" className="btn btn-dark">
+          Browse {items.length} Cars
+        </Link>
+        <Link to="/new" className="btn btn-outline-dark">
+          + Add New Car
+        </Link>
+       </div>
     </div>
   )
 }
