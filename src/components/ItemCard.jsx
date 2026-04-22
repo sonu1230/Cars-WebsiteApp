27a4305ec06a0ea684792e241b17822886149304
Sonu <sonukumari.mahatopanjiyar@dcmail.ca>
2026-04-21 20:49:27 -0400

    Maps category
    Maps category name so each car type has a visually distinct label

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,4 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useItemsContext } from '../context/ItemsContext'
+
+//Maps category nae to Bootstrap badge color class so each car type has a visaully distinct label
+const badgeColor = {
+  Muscle:   'danger',
+  Sports:   'primary',
+  Supercar: 'warning',
+  Hypercar: 'dark',
+  EV:       'success',
+  GT:       'info',
+  Hybrid:   'secondary'
+}
+
 export default function ItemCard(/* { item, onView, onEdit, onDelete } */){
   return (
     <div className="card h-100">
