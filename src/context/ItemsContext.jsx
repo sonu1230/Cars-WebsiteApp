32ad8689389dea823ad456d027ed2e67ed609463
Sonu <sonukumari.mahatopanjiyar@dcmail.ca>
2026-04-21 20:09:04 -0400

    Updating ItemsContext
    Importing useItems and hook  and complete items context by wiring use items hook and adding useItemsContext consumer

--- src/context/ItemsContext.jsx
+++ src/context/ItemsContext.jsx
@@ -1,13 +1,17 @@
 import React, { createContext } from 'react'
+import useItems from '../hooks/useItems'
+
 
 export const ItemsContext = createContext(null)
 
 export function ItemsProvider({ children }) {
-  // TODO: initialize state and handlers (consider a custom useItems hook)
+  // initialize state and handlers (consider a custom useItems hook)
 
-  const value = {
-    // TODO: expose items, derived list, filter state, CRUD handlers
-  }
-
+  const value = useItems()
+   
   return <ItemsContext.Provider value={value}>{children}</ItemsContext.Provider>
+}
+ //UseContext(ItemsContext) directly in everye view component
+export function useItemsContext() {
+  return useContext(ItemsContext)
 }
